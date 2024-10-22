7a443df672f0988d23105718dc3d546b52c39655
Futu4029 <nofuture.gonza@gmail.com>
2024-10-21 23:27:45 -0300

    Entrega 2 - agrego el user al dto de application

--- src/main/java/com/unq/adopt_me/dto/application/ApplicationDto.java
+++ src/main/java/com/unq/adopt_me/dto/application/ApplicationDto.java
@@ -2,6 +2,7 @@ package com.unq.adopt_me.dto.application;
 
 import com.unq.adopt_me.entity.adoption.Adoption;
 import com.unq.adopt_me.entity.adoption.Application;
+import com.unq.adopt_me.entity.user.User;
 import com.unq.adopt_me.util.ApplicationStatus;
 
 public class ApplicationDto {
@@ -10,8 +11,11 @@ public class ApplicationDto {
 
     public ApplicationStatus applicationStatus;
 
+    public User user;
+
     public ApplicationDto(Application application) {
         this.adoption = application.getAdoption();
         this.applicationStatus = application.getApplicationStatus();
+        this.user = application.getAdopter();
     }
 }
