9565fedd0bdbe33e500083dfa276ecf83498248c
Futu4029 <nofuture.gonza@gmail.com>
2024-11-23 10:16:21 -0300

    key dto

---
+++ src/main/java/com/unq/adopt_me/dto/notification/Keys.java
@@ -0,0 +1,23 @@
+package com.unq.adopt_me.dto.notification;
+
+public class Keys {
+    private String p256dh;
+    private String auth;
+
+    // Getters y Setters
+    public String getP256dh() {
+        return p256dh;
+    }
+
+    public void setP256dh(String p256dh) {
+        this.p256dh = p256dh;
+    }
+
+    public String getAuth() {
+        return auth;
+    }
+
+    public void setAuth(String auth) {
+        this.auth = auth;
+    }
+}
